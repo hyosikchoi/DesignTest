2af6db5c22624d8eec523163e7a784b6d89174a6
hyosikchoi <huchoi24@naver.com>
2024-04-22 22:10:42 +0900

    Add - annotation DigitTextView

--- app/src/main/java/com/example/designtestapplication/DigitTextView.kt
+++ app/src/main/java/com/example/designtestapplication/DigitTextView.kt
@@ -19,6 +19,8 @@ class DigitTextView @JvmOverloads constructor(
 
     companion object {
         private const val ANIMATION_DURATION = 100
+
+        private const val INTERVAL = 1
     }
 
     var currentTextView: TextView? = null
@@ -38,20 +40,27 @@ class DigitTextView @JvmOverloads constructor(
                 String.format(Locale.getDefault(), "%d", desiredValue)
         }
         val oldValue = currentTextView?.text.toString().toInt()
+        // 현재 값이 새로 입력된 값보다 크면
         if (oldValue > desiredValue) {
-            nextTextView?.text = String.format(Locale.getDefault(), "%d", oldValue - 1)
+            nextTextView?.text = String.format(Locale.getDefault(), "%d", oldValue - INTERVAL)
+            // translationY > 0 면 아래로, translationY < 0 면 위로
+            // 현재 값을 위로 올리는 애니메이션을 취하고
             currentTextView?.animate()?.translationY(-height.toFloat())?.setDuration(
                 ANIMATION_DURATION.toLong()
             )?.start()
+
             nextTextView?.translationY = nextTextView?.height?.toFloat() ?: 0f
+            // 현재 값-interval 한 text를 제자리로 올리는 애니메이션을 취한다.
             nextTextView?.animate()?.translationY(0f)?.setDuration(ANIMATION_DURATION.toLong())
                 ?.setListener(object : Animator.AnimatorListener {
                     override fun onAnimationStart(animation: Animator) {}
                     override fun onAnimationEnd(animation: Animator) {
+                        // 애니메이션 종료 시 현재 값에 - interval 하고 nextTextView 와 다시 겹쳐 놓는다.
                         currentTextView?.text =
-                            String.format(Locale.getDefault(), "%d", oldValue - 1)
+                            String.format(Locale.getDefault(), "%d", oldValue - INTERVAL)
                         currentTextView?.translationY = 0f
-                        if (oldValue - 1 != desiredValue) {
+                        // 구간 만큼 차감 시 아직 입력값이 아니라면 재귀를 돌린다.
+                        if (oldValue - INTERVAL != desiredValue) {
                             setValue(desiredValue)
                         }
                     }
@@ -60,7 +69,7 @@ class DigitTextView @JvmOverloads constructor(
                     override fun onAnimationRepeat(animation: Animator) {}
                 })?.start()
         } else if (oldValue < desiredValue) {
-            nextTextView?.text = String.format(Locale.getDefault(), "%d", oldValue + 1)
+            nextTextView?.text = String.format(Locale.getDefault(), "%d", oldValue + INTERVAL)
             currentTextView?.animate()?.translationY(height.toFloat())?.setDuration(
                 ANIMATION_DURATION.toLong()
             )?.start()
@@ -70,9 +79,9 @@ class DigitTextView @JvmOverloads constructor(
                     override fun onAnimationStart(animation: Animator) {}
                     override fun onAnimationEnd(animation: Animator) {
                         currentTextView?.text =
-                            String.format(Locale.getDefault(), "%d", oldValue + 1)
+                            String.format(Locale.getDefault(), "%d", oldValue + INTERVAL)
                         currentTextView?.translationY = 0f
-                        if (oldValue + 1 != desiredValue) {
+                        if (oldValue + INTERVAL != desiredValue) {
                             setValue(desiredValue)
                         }
                     }
